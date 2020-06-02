95ddd04e0174c9293226ddaffabb5affb3b24ff2
Arony Salvador <salvadorarony@gmail.com>
2020-06-01 23:05:06 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const  express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+//crear el Servidor
+const app = express();
+
+//Conectar a la Base de Datos
+conectarDB();
+
+//Habilitar CORS
+app.use(cors());
+
+//Habilitar Express.json
+app.use(express.json({ extended: true}));
+
+//Puerto de la App
+const PORT = process.env.PORT || 4000;
+
+//Importar Rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/tareas', require('./routes/tareas'));
+
+//Arracar la app
+app.listen(PORT,() =>{
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+});
